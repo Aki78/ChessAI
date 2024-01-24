d5260a653c0900f06a2577e22ae0fd52c7d28efc
Aki78 <kmorooka78@gmail.com>
2024-01-24 20:42:54 +0200

    done mostly pieces.h

--- pieces.h
+++ pieces.h
@@ -1,11 +1,24 @@
-
 class Piece {
 public:
+	Piece(int row, int column, char color) : current_row(row), current_column(column), piece_color(color) {}
 
-	void temp_row;
-	void temp_column;
-	void make_raw_move();
+	int get_row() const { return current_row; }
+	int get_column() const { return current_column; }
 
+	int power; // Like queen is 9 or 10
 
+	char get_color() const { return piece_color; }
+
+	virtual void make_move(int target_row, int target_column) = 0;
+
+protected:
+	
+	int current_row;
+	int current_column;
+	
+	char piece_color;
+
+	
 };
+
 
