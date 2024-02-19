0494fb83efec822de1b3455a1d4d2841a6ab34ca
Aki78 <kmorooka78@gmail.com>
2024-02-19 19:09:13 +0200

    minimax algorithm values seem to work, but can't get the move

--- main.cpp
+++ main.cpp
@@ -9,12 +9,126 @@
 
 using namespace std;
 
+Move find_best_greedy_move(vector<Move> moves, Position position){
+
+	Move maxMove(0,0,0,0);
+	int maxValue = -1000; // Include <climits> for INT_MIN
+	map<Move, int> action_value;
+
+	for(auto m: moves){ // trying out moves to get state action values
+		Position test_position = position; // copying new position for test
+		test_position.make_move(m);
+		vector<int> state_values = test_position.get_state_value();
+		int state_value = 0;
+		if(position._turn == WHITE) state_value = state_values[0] - state_values[1];
+		else if(position._turn == BLACK) state_value = state_values[1] - state_values[0];
+		cout << "values are: " << state_values[0] << " " << state_values[1] << endl;
+		cout << "action is: " << endl;
+		m.print_move();
+		cout << "state value is: " << state_value << endl;
+		action_value[m] = state_value;
+		cout << "size is: " << action_value.size();
+	}
+
+	for (const auto& pair : action_value) {
+		cout << pair.second << endl;
+		if (pair.second > maxValue) {
+			maxValue = pair.second;
+			maxMove = pair.first;
+			maxMove.print_move();
+		}
+	}
+
+	return maxMove;
+}
+
+
+map<Move, int> get_max(map<Move, int> a, map<Move, int> b){
+
+	int valA = a.begin() -> second;
+	int valB = b.begin() -> second;
+
+	if(valA > valB) return a;
+	else return b;
+
+
+
+}
+
+map<Move, int> get_min(map<Move, int> a, map<Move, int> b){
+
+	int valA = a.begin() -> second;
+	int valB = b.begin() -> second;
+
+	cout << "VAL A " << valA << endl;
+
+	if(valA < valB) return a;
+	else return b;
+
+}
+
+
+map<Move, int> minimax(Move move, Position position, int depth, bool maximizingPlayer){ // move is just a place holder at start
+	if(depth == 0){
+		vector<int> state_values = position.get_state_value(); // split to white and black
+		map<Move, int> action_value;
+		int state_value = 0;
+
+		if(position._turn == WHITE) state_value = state_values[0] - state_values[1];
+		else if(position._turn == BLACK) state_value = state_values[1] - state_values[0];
+
+		action_value[move] = state_value;
+		cout << "Max depth reached: " << state_value << endl;
+
+		return action_value; 
+	}
+
+	vector<Move> moves = position.generate_legal_moves();
+	cout << "starting evaluation" << endl;
+	if(maximizingPlayer){
+		int maxEval = -999;
+		Move init_move(0,0,0,0);
+
+		map<Move, int> maxMove;
+		maxMove[init_move] = maxEval;
+		for(auto m: moves){ //getting all legal moves (children)
+			Position test_position = position; 
+			test_position.make_move(m);
+			map<Move, int> moveEval = minimax(m, test_position, depth - 1, false);
+			maxMove = get_max(maxMove, moveEval);
+		}
+
+		return maxMove;
+	}
+	if(!maximizingPlayer){
+		int minEval = +999;
+		Move init_move(0,0,0,0);
+
+		map<Move, int> minMove;
+		minMove[init_move] = minEval;
+
+		for(auto m: moves){ //getting all legal moves (children)
+			Position test_position = position; 
+			test_position.make_move(m);
+			map<Move, int> moveEval = minimax(m, test_position, depth - 1, true);
+			minMove = get_min(minMove, moveEval);
+		}
+
+		return minMove;
+	}
+
+
+}
+
+
+
+
 int main(){
         // Initial position.
         Position position;
 	vector<Move> moves;
 	moves = position.generate_legal_moves();
-	map<Move, int> action_value;
+//	map<Move, int> action_value;
 	
 
 
@@ -22,7 +136,7 @@ int main(){
 		string input_move_string;
 		cout << "Printing out all moves: " << endl;
 		for(auto m: moves) m.print_move();
-		action_value.clear();
+//		action_value.clear();
 
                 position.print();
 		cout << "Input your move: ";
@@ -33,32 +147,9 @@ int main(){
 //		if (!count(moves.begin(),moves.end(), new_move)) continue; // checking if moves exist
 		if (!count(moves.begin(),moves.end(), new_move)) cout << "GAME OVER!!!!"; // checking if moves exist
 
-		for(auto m: moves){ // trying out moves to get state action values
-			Position test_position = position; // copying new position for test
-			test_position.make_move(m);
-			vector<int> state_values = test_position.get_state_value();
-			int state_value = 0;
-			if(position._turn == WHITE) state_value = state_values[0] - state_values[1];
-			else if(position._turn == BLACK) state_value = state_values[1] - state_values[0];
-			cout << "values are: " << state_values[0] << " " << state_values[1] << endl;
-			cout << "action is: " << endl;
-			m.print_move();
-			cout << "state value is: " << state_value << endl;
-			action_value[m] = state_value;
-			cout << "size is: " << action_value.size();
-		}
-
-		Move maxMove(0,0,0,0);
-		int maxValue = -1000; // Include <climits> for INT_MIN
-
-		for (const auto& pair : action_value) {
-			cout << pair.second << endl;
-			if (pair.second > maxValue) {
-				maxValue = pair.second;
-				maxMove = pair.first;
-				maxMove.print_move();
-			}
-		}
+//		Move maxMove = find_best_move(moves, position);
+		map<Move, int> maxMoveMap = minimax(Move(1,1,1,1), position, 4, true);
+		Move maxMove = maxMoveMap.begin() -> first;
 
 		cout << "MAX MOVE MADE:::" << endl;
 		maxMove.print_move();
