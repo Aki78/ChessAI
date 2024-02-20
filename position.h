267ee6df1453d59429e2336611605dc7b031c059
Aki78 <kmorooka78@gmail.com>
2024-02-20 20:47:11 +0200

    reorganized chess position

--- position.h
+++ position.h
@@ -62,8 +62,8 @@ private:
 		{ bP, bP, bP, bP, bP, bP, bP, bP },
 		{ NA, NA, NA, NA, NA, NA, NA, NA },
 		{ NA, NA, NA, NA, NA, NA, NA, NA },
-		{ NA, NA, wQ, NA, NA, NA, bQ, NA },
-		{ NA, NA, NA, NA, NA, wP, NA, NA },
+		{ NA, NA, NA, NA, NA, NA, NA, NA },
+		{ NA, NA, NA, NA, NA, NA, NA, NA },
 		{ wP, wP, wP, wP, wP, wP, wP, wP },
 		{ wR, wN, wB, wQ, wK, wB, wN, wR }
 	};
