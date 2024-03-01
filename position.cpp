1cb5eefbc26f25c0081b08b6af9200771d997e65
Aki78 <kmorooka78@gmail.com>
2024-03-01 19:23:39 +0200

    pawn bug fixed

--- position.cpp
+++ position.cpp
@@ -187,7 +187,7 @@ void Position::check_pawn_and_push_move(int row, int column, int player, vector<
 		current_row = row; // resetting position
 
 		// if enemy on sides
-		if(current_column - 1 > 0)
+		if(current_column - 1 >= 0)
 			if (is_enemy_of_white(_board[current_row-1][current_column-1])){ 
 				moves.push_back(Move(row, column, current_row-1, current_column-1));
 			}
@@ -230,7 +230,7 @@ void Position::check_pawn_and_push_move(int row, int column, int player, vector<
 		current_row = row; // resetting position
 
 		// if enemy on sides
-		if(current_column - 1 > 0)
+		if(current_column - 1 >= 0)
 			if (is_enemy_of_black(_board[current_row+1][current_column-1])){ 
 				moves.push_back(Move(row, column, current_row+1, current_column-1));
 			}
